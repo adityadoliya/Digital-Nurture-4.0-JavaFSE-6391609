d7d1f683101045f2efc1a71a41bb1efc3ea6be4d
Aditya Doliya <adityadoliya54@gmail.com>
2025-07-09 01:25:41 +0530

    Updated CalculatorTest with my Superset ID 6391609

--- week2/TDD junit mockito/Setting Up JUnit/CalculatorTest.java
+++ week2/TDD junit mockito/Setting Up JUnit/CalculatorTest.java
@@ -5,12 +5,14 @@ public class CalculatorTest {
 
     @Test
     public void testAddition() {
-        int result = 6064 + 513; //SupersetID-6064513
-        assertEquals(6577, result);
+        int result = 6391 + 609; // SupersetID-6391609
+        assertEquals(7000, result);
     }
+
     @Test
-    public void testSubraction(){
-        int result=6064-513;
-        assertEquals("Subraction of Superset ID",5551,result);
+    public void testSubtraction() {
+        int result = 6391 - 609;
+        assertEquals("Subtraction of Superset ID", 5782, result);
     }
 }
+
