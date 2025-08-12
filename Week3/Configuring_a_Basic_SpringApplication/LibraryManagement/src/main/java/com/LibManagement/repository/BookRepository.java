1f9cfa739255c31b508c6ac4d637934b16757b1a
Aditya Doliya <adityadoliya54@gmail.com>
2025-08-12 12:08:58 +0530

    made new files while doing revison for the test so uploading new files

--- Week3/Configuring_a_Basic_SpringApplication/LibraryManagement/src/main/java/com/LibManagement/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.LibManagement.repository;
-
-public class BookRepository {
-    public void repo(){
-        System.out.println("Book Repository");
-    }
-}
--- Week3/Configuring_a_Basic_SpringApplication/LibraryManagement/src/main/java/com/LibManagement/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.LibManagement.service;
-
-public class BookService {
-    public void service(){
-        System.out.println("Book Service");
-    }
-}
